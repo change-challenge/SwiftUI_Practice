834462306e3e93fb7276e3a2a3e81d8ea369b1e1
change-challenge <dog941128@hotmail.com>
2022-04-18 22:17:15 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hojin Jang on 2022/04/18.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
