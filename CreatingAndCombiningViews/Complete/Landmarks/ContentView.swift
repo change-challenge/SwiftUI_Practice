0c0f08005399e7f7366502ce0bbc7611cac13461
change-challenge <dog941128@hotmail.com>
2022-04-22 23:25:58 +0900

    0422

--- CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
+++ CreatingAndCombiningViews/Complete/Landmarks/ContentView.swift
@@ -1,39 +1,43 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Hojin Jang on 2022/04/18.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A view showing the details for a landmark.
+*/
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
+        VStack {
             MapView()
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
+
             CircleImage()
                 .offset(y: -130)
                 .padding(.bottom, -130)
+
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
+
                 HStack {
                     Text("Joshua Tree National Park")
-                        .font(.subheadline)
                     Spacer()
                     Text("California")
-                        .font(.subheadline)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
+
                 Divider()
+
                 Text("About Turtle Rock")
                     .font(.title2)
                 Text("Descriptive text goes here.")
             }
             .padding()
+
             Spacer()
         }
     }
---
+++ Landmarks/Landmark.swift
@@ -0,0 +1,30 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Hojin Jang on 2022/04/22.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longtitude: Double
+    }
+}
